4e34ab0e91f2d91faa20e1b40c9948cec3dfccb9
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-06-06 15:57:35 -0500

    adding some styling brought in by bootstrap template

---
+++ src/main/java/com/codeup/plantapp/PlantAppApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.plantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/plantapp/PlantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.plantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
