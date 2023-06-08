4f83ac3c640c4d2fc3018c7596edc8c59ee14bbc
Quintyn Williams <t.quin.williams@gmail.com>
2023-06-08 11:51:24 -0500

    stashing

--- src/main/java/com/codeup/plantapp/repositories/GardenPlant.java
+++
@@ -1,8 +0,0 @@
-package com.codeup.plantapp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GardenPlant extends JpaRepository<GardenPlant, Long> {
-
-        GardenPlant findById(long id);
-}
