4cb29f21fc577a228f8c9cb13d86d25c96cc2eb2
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-06-12 13:53:54 -0500

    adding styling and some functionality to edit user, create user, css

---
+++ src/main/resources/static/js/staticElements.js
@@ -0,0 +1,7 @@
+let loadFile = function(event) {
+    let output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+        URL.revokeObjectURL(output.src) // free memory
+    }
+};
