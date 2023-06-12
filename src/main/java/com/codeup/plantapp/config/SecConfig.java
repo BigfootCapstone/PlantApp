22fb2d691664c35112db00a4ea6b45e203f28c10
Sarah Brightman <sarah.graciela.brightman@gmail.com>
2023-06-12 10:14:16 -0500

    security update

--- src/main/java/com/codeup/plantapp/config/SecConfig.java
+++ src/main/java/com/codeup/plantapp/config/SecConfig.java
@@ -1,8 +1,12 @@
 package com.codeup.plantapp.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -11,8 +15,22 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class SecConfig {
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnDefaultWebSecurity
+    static class SecurityFilterChainConfiguration {
+
+        @Bean
+        @Order(SecurityProperties.BASIC_AUTH_ORDER)
+        SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+            http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+            http.formLogin(withDefaults());
+            http.httpBasic(withDefaults());
+            return http.build();
+        }
+    }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
