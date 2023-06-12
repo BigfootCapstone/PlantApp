1171b9d31c77b406c5503880448a12846de65b99
Sarah Brightman <sarah.graciela.brightman@gmail.com>
2023-06-12 12:22:15 -0500

    security login user done

--- src/main/java/com/codeup/plantapp/config/SecConfig.java
+++ src/main/java/com/codeup/plantapp/config/SecConfig.java
@@ -1,50 +1,38 @@
 package com.codeup.plantapp.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration(proxyBeanMethods = false)
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class SecConfig {
-    @Configuration(proxyBeanMethods = false)
-    @ConditionalOnDefaultWebSecurity
-    static class SecurityFilterChainConfiguration {
 
-        @Bean
-        @Order(SecurityProperties.BASIC_AUTH_ORDER)
-        SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-            http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-            http.formLogin(withDefaults());
-            http.httpBasic(withDefaults());
-            return http.build();
-        }
-    }
+
+@Configuration
+public class SecConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll()
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/**").permitAll()
+                .requestMatchers("/search","/users/*", "weather").authenticated()
+                .requestMatchers("/search", "/users/*", "/register", "/users/login","/").permitAll()
+                .requestMatchers("/css/**", "/js/**").permitAll()
         );
 
-        http.formLogin((form) -> form.loginPage("/login").defaultSuccessUrl("/"));
-        http.logout((form) -> form.logoutSuccessUrl("/login"));
+        http.formLogin((form) -> form.loginPage("/users/login").defaultSuccessUrl("/users/profile"));
+        http.logout((form) -> form.logoutSuccessUrl("/"));
         http.httpBasic(withDefaults());
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder () {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/codeup/plantapp/controllers/UserController.java
+++ src/main/java/com/codeup/plantapp/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.codeup.plantapp.models.User;
 import com.codeup.plantapp.repositories.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,13 @@ public class UserController {
 
     private final UserRepository usersDao;
 
-    public UserController(UserRepository usersDao) {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userDao;
+
+    public UserController(UserRepository userDao, UserRepository usersDao, PasswordEncoder passwordEncoder){
         this.usersDao = usersDao;
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/create")
@@ -27,18 +33,29 @@ public class UserController {
     //messing with it for the create user
     @PostMapping("/create")
     public String createUserProfile(@ModelAttribute("user") User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         usersDao.save(user);
         return "redirect:/userProfile";
     }
     @GetMapping("/login")
-    public String viewLoginPage(@PathVariable Long id) {
-        User user = usersDao.findById(id)
-        .formLogin()
-                .loginPage("/login")
-                .usernameParameter("email");
-        return "/login";
+    public String viewLoginPage() {
+        return "login";
     }
-
+    @PostMapping("/login")
+    public String loginSessionSetter(Model model, HttpSession session){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        session.setAttribute("user", user);
+        return "userProfile";
+    }
+    @GetMapping("/profile")
+    public String showProfile(Model model){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        long userId = user.getId();
+        user = userDao.findUserById(userId);
+        model.addAttribute("user", user);
+        System.out.println(user.getUsername());
+        return "userProfile";
+    }
     @GetMapping("/{id}")
     public String getUserProfile(@PathVariable Long id, Model model) {
         User user = usersDao.findById(id)
