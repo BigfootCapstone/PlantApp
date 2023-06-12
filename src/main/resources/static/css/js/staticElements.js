4ff5cb7b4eceb7e81af824ba683284c2e7fab8f2
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-06-12 18:46:39 -0500

    resolving edit feature

--- src/main/resources/static/css/js/staticElements.js
+++ src/main/resources/static/css/js/staticElements.js
@@ -5,3 +5,11 @@ let loadFile = function(event) {
         URL.revokeObjectURL(output.src) // free memory
     }
 };
+
+let reader = new FileReader();
+reader.onload = function (r_event) {
+    document.getElementById('profile-img').setAttribute('src', r_event.target.result);
+}
+document.getElementsByName('image')[0].addEventListener('change', function (event){
+    reader.readAsDataURL(this.files[0]);
+});
