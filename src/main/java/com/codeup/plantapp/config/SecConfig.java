55c15b134a782e18c557b825fb2e9472adcd806a
Sarah Brightman <sarah.graciela.brightman@gmail.com>
2023-06-20 10:54:20 -0500

    still working on bugs trying to stay up to date with main

--- src/main/java/com/codeup/plantapp/config/SecConfig.java
+++ src/main/java/com/codeup/plantapp/config/SecConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,7 +16,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
-@EnableWebSecurity
 public class SecConfig {
 
     @Bean
