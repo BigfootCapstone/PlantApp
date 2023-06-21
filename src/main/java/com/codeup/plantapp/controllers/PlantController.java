8954c0d18e02ea6d84a3ab2d5cd78615726d2841
Quintyn Williams <t.quin.williams@gmail.com>
2023-06-21 12:51:11 -0500

    server runtime plant check and emailer for all users

--- src/main/java/com/codeup/plantapp/controllers/PlantController.java
+++ src/main/java/com/codeup/plantapp/controllers/PlantController.java
@@ -11,7 +11,6 @@ import com.codeup.plantapp.util.PlantResultDTO;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +20,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 import static com.codeup.plantapp.services.PlantsCall.*;
--- src/main/java/com/codeup/plantapp/services/EmailService.java
+++ src/main/java/com/codeup/plantapp/services/EmailService.java
@@ -1,6 +1,7 @@
 package com.codeup.plantapp.services;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.amazonaws.auth.*;
 import com.amazonaws.regions.Regions;
@@ -11,17 +12,22 @@ import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.codeup.plantapp.models.GardenPlant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
 
-    public static void sendSimpleMessage(String accessKey, String secretKey) throws IOException {
+    public static void sendSimpleMessage(String accessKey, String secretKey, String userEmail,
+                                         List<GardenPlant> userPlants) throws IOException {
         // Replace sender@example.com with your "From" address.
         // This address must be verified with Amazon SES.
         String FROM = "botanibuddy@gmail.com";
 
         // Replace recipient@example.com with a "To" address. If your account
         // is still in the sandbox, this address must be verified.
-        String TO = "quintyn96@gmail.com";
+//        String TO = userEmail;
 
         // The configuration set to use for this email. If you do not want to use a
         // configuration set, comment the following variable and the
@@ -32,10 +38,17 @@ public class EmailService {
         String SUBJECT = "Amazon SES test (AWS SDK for Java)";
 
         // The HTML body for the email.
-        String HTMLBODY = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
-                + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
-                + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
-                + "AWS SDK for Java</a>";
+        StringBuilder HTMLBODY =
+                new StringBuilder("<h1>Hello from BotaniBuddy,</h1>"
+                        + "Your plant(s) are thirsty!");
+
+        for (GardenPlant plant : userPlants) {
+            String name = plant.getPlant().getName();
+            String water = plant.getWater_tip();
+            String plantNotify = "<p>" + name + water + "</p>";
+            HTMLBODY.append(plantNotify);
+        }
+
 
         // The email body for recipients with non-HTML email clients.
         String TEXTBODY = "This email was sent through Amazon SES "
@@ -47,11 +60,11 @@ public class EmailService {
                     .withRegion(Regions.US_EAST_2)
                     .build();
             SendEmailRequest request = new SendEmailRequest()
-                    .withDestination(new Destination().withToAddresses(TO))
+                    .withDestination(new Destination().withToAddresses(userEmail))
                     .withMessage(new Message()
                             .withBody(new Body()
                                     .withHtml(new Content()
-                                            .withCharset("UTF-8").withData(HTMLBODY))
+                                            .withCharset("UTF-8").withData(HTMLBODY.toString()))
                                     .withText(new Content()
                                             .withCharset("UTF-8").withData(TEXTBODY)))
                             .withSubject(new Content()
