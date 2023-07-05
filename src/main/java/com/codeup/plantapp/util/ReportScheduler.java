e28e6d4a5e280d29fb833ba43ba69cd38265db36
Quintyn Williams <t.quin.williams@gmail.com>
2023-07-05 09:11:13 -0500

    live service

--- src/main/java/com/codeup/plantapp/util/ReportScheduler.java
+++ src/main/java/com/codeup/plantapp/util/ReportScheduler.java
@@ -35,39 +35,31 @@ public class ReportScheduler {
 
 //    https://cronitor.io/guides/java-cron-jobs
 //    milliDay 86400000
-//    @Scheduled(fixedDelay = 86400000)
-//    public void sitePlantChecker() throws Exception {
-//
-//        List<User> botaniBuddies = usersDao.findAll();
-//
-//        for (User botaniUser: botaniBuddies) {
-//
-//            String city = botaniUser.getCity();
-//            String weatherKey = keys.getOpenWeather();
-//            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey +
-//                    "&units=imperial");
-//            Weather localWeather = getWeather(url);
-//
-//            List<GardenPlant> userGarden = gardenPlantDao.findGardenPlantByUser(botaniUser);
-//
-//            List<GardenPlant> endangeredPlants = new ArrayList<>();
-//
-//            for (GardenPlant plant: userGarden) {
-//                GardenPlant foundPlant = gardenPlantDao.findGardenPlantsById(plant.getId());
-//                GardenPlant checkedPlant = plantTipCheck(foundPlant, localWeather);
-//
-//                gardenPlantDao.save(checkedPlant);
-//
-//                if (checkedPlant.getWater_in_days() == 0){
-//                    endangeredPlants.add(checkedPlant);
-//                }
-//            }
-//
-//            if (botaniUser.getIs_emailNotifiable()){
-//                String email = botaniUser.getEmail();
-//                sendSimpleMessage(keys.getAwsSES(), keys.getAwsSESSecret(), email, endangeredPlants);
-//            }
-//        }
-//
-//    }
+    @Scheduled(fixedDelay = 86400000)
+    public void sitePlantChecker() throws Exception {
+
+        List<User> botaniBuddies = usersDao.findAll();
+
+        for (User botaniUser: botaniBuddies) {
+
+            String city = botaniUser.getCity();
+            String weatherKey = keys.getOpenWeather();
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey +
+                    "&units=imperial");
+            Weather localWeather = getWeather(url);
+
+            List<GardenPlant> userGarden = gardenPlantDao.findGardenPlantByUser(botaniUser);
+
+
+            for (GardenPlant plant: userGarden) {
+                GardenPlant foundPlant = gardenPlantDao.findGardenPlantsById(plant.getId());
+                GardenPlant checkedPlant = plantTipCheck(foundPlant, localWeather);
+
+                gardenPlantDao.save(checkedPlant);
+
+            }
+
+        }
+
+    }
 }
