cd08caad8011f8d1147e95b3faaf630cf1ed962d
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-07-05 11:22:42 -0500

    getting rid of all unused icons, and removing pe7 and material design icons to be replaced with custom ones or bootstrap ones

--- src/main/resources/static/css/js/staticElements.js
+++ src/main/resources/static/css/js/staticElements.js
@@ -1,12 +1,4 @@
-let reader = new FileReader();
-reader.onload = function (r_event) {
-    document.getElementById('profile-img').setAttribute('src', r_event.target.result);
-}
-document.getElementsByName('image')[0].addEventListener('change', function (event){
-    reader.readAsDataURL(this.files[0]);
-});
-
-
+/********BOOTSTRAP Tabs**************/
 
 const triggerTabList = document.querySelectorAll('#myTab button')
 triggerTabList.forEach(triggerEl => {
